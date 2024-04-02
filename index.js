f542059fee6de16d8993ccaa7cdf8c4eea7f9790
stephen owino <Sowino939@gmail.com>
2024-04-02 13:15:22 +0300

    create loop file

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+// Code your solutions in this
+function writeCards(array, name) {
+  let emptyArrays =[];
+  for (let i= 0; i < array.length; i++) {
+    let emptyArray =`Thank you, ${array[i]}, for the wonderful ${name} gift!`;
+    emptyArrays.push(emptyArray);
+    debugger;
+  }
+  return emptyArrays;
+}
+function countDown(num) {
+  while (num>=0) {
+    console.log(num--);
+  }
+}
+console.log(countDown(10))
